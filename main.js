2ceeee4bd902e8b55ac9c46bb27e12d1d796a86a
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-24 17:46:44 +0100

    finito!

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-function init() {
-    $('nav li').hover(function(){
-        $('.contesto.prova').removeClass('prova');
-        $(this).find('.contesto').addClass('prova');
-    }, function(){
-    })
+function clickRemove(){
+
     $('body').click(function(){
         $('.contesto.prova').removeClass('prova');
     })
     $('.contesto').click(function(event){
-
+    
         event.stopPropagation()
+    })
+
+}
+function fhover() {
+
+    $('nav li').hover(function(){
+        $('.contesto.prova').removeClass('prova');
+        $(this).find('.contesto').addClass('prova');
+    },)
+}
+function init() {
+    fhover();
+    clickRemove()
+    $('nav li').click(function(){
+        var prova=$(this).find('.contesto').attr('class')
+        
+        if(prova.includes('prova')){
+            console.log('ciao')
+        }else {
+            console.log('ocaca')
+            $(this).find('.contesto').addClass('prova');
+            event.stopPropagation()
+        }
     })
 }
 $(document).ready(init)
