fab3970532ac0daa1b5032d98edba5410cf53409
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-24 18:05:02 +0100

    FIne esercizio

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 function clickRemove(){
-
     $('body').click(function(){
         $('.contesto.prova').removeClass('prova');
     })
     $('.contesto').click(function(event){
-    
         event.stopPropagation()
     })
-
 }
 function fhover() {
 
@@ -19,16 +16,19 @@ function fhover() {
 function init() {
     fhover();
     clickRemove()
-    $('nav li').click(function(){
+    $('nav li').click(function(event){
         var prova=$(this).find('.contesto').attr('class')
         
         if(prova.includes('prova')){
             console.log('ciao')
         }else {
-            console.log('ocaca')
+            console.log('provaaa')
             $(this).find('.contesto').addClass('prova');
             event.stopPropagation()
+
         }
     })
 }
 $(document).ready(init)
+
+//aggiungere e rimuovere un evento
